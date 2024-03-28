ed316aa49ccd534224fc12e56b7de2ad2623d253
SNinjo <70991369+SNinjo@users.noreply.github.com>
2024-03-28 15:51:33 +0800

    update

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-16926f266be66a4ec0c5.js"
+    "url": "webpack-runtime-04fc75c2561189d5f566.js"
   },
   {
     "url": "framework-ca6554274ce44d943dae.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bae46387417d9ba27d35be0572a360e8"
+    "revision": "c9f18e501c22a18dd8c2d63951ecf4ec"
   },
   {
     "url": "manifest.webmanifest",
